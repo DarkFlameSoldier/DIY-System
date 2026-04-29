4102a494eeba76ac7185604fe559946508697977
IvoDimov <s23521507@onlineedu.tu-varna.bg>
2026-04-29 14:57:11 +0300

    add password field to Users table

--- DIY-System/Form1.cs
+++ DIY-System/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace DIY_System
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlconnection;
+        SqlCommand sqlcommand;
+        string query;
+        SqlDataAdapter sqladapter;
+        string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivo\source\repos\DIY-System\DIY-System\DIY.mdf;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
